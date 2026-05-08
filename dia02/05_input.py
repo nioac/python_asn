821ffc8dcdc56d344a99016a69cd55268bd7360d
paulo <paulonioac@gmail.com>
2026-05-07 21:59:21 -0300

    começo de exercicios

---
+++ dia02/05_input.py
@@ -0,0 +1,15 @@
+# %%
+
+nome = input("Digite seu nome: ")
+# print(f"Olá, {nome}!")
+
+# %%
+
+idade = input("Digite sua idade: ")
+# print(idade, type(idade))  
+
+idade = int(idade)
+# print(idade, type(idade))   
+
+diff = 75 - idade
+print(f"Olá {nome}! Faltam {diff} anos para você completar 75 anos.")
---
+++ exercicios/03_exercicio.py
@@ -0,0 +1,51 @@
+# %%
+
+input()
+print("""
+      Aqui começa uma historia...""")
+
+input()
+print("""
+      era uma vez um jovem chamado João, 
+      que vivia em uma pequena vila cercada por montanhas.""")
+
+input()
+print("""
+      João era conhecido por sua curiosidade e espírito aventureiro, 
+      sempre buscando explorar o mundo ao seu redor.""")
+
+input()
+print("""
+      Um dia, ele decidiu escalar a montanha mais alta da região, 
+      em busca de novas experiências e desafios.""")
+
+input()
+print("""
+      Durante a escalada, João enfrentou ventos fortes, 
+      terrenos acidentados e até mesmo uma tempestade repentina.""")
+
+input()
+print("""
+      Mas ele não desistiu, continuou subindo com determinação e coragem.""")
+
+input()
+print("""
+      Finalmente, após horas de esforço, João alcançou o 
+      topo da montanha e foi recompensado com uma vista deslumbrante do mundo abaixo.""")
+
+input()
+print("""
+      Ele se sentiu realizado e grato por ter 
+      superado os obstáculos e alcançado seu objetivo.""")
+
+input()
+print("""
+      Essa experiência ensinou a João a importância de 
+      perseverar diante das dificuldades e a valorizar as conquistas, por menores que sejam.""")
+
+input()
+print("""
+      E assim, a história de João se tornou uma inspiração 
+      para todos na vila, lembrando-os de que, com 
+      coragem e determinação, é possível alcançar grandes coisas na vida.""")
+

712494ecd888e9371b08c261c97bdd48c0596a42
paulo <paulonioac@gmail.com>
2026-05-21 22:33:23 -0300

    nada de mais

---
+++ exercicios/ex_06.py
@@ -0,0 +1,9 @@
+# %%
+
+
+# praticando o uso de funções
+
+def saudacao(nome: str):
+    print(f"Olá {nome}, seja bem vindo!")       
+        ola = "Olá"
+        print(f"{ola} {nome}, seja bem vindo!")     
